23d2ae97ed7aedcdca98d832a12c281e7509b79c
liangyancheng <656040874@qq.com>
2024-06-28 10:23:24 +0800

    build: 测试 代码提交

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.less';
 const App: React.FC = () => {
   return (
     <div className="wrapper">
-      hello Ryan lyc 测试 代码提交
+      hello Ryan lyc 测试代码提交
       <img src={image} width={200} height={100} />
     </div>
   );
